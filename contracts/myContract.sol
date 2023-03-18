e145876a7d11cee044aed78c875afdfac3f98d9c
Narongchai Krutcham <narongchai.k63@rsu.ac.th>
2023-03-18 12:49:44 +0700

    1

--- contracts/myContract.sol
+++ contracts/myContract.sol
@@ -1,18 +1,36 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.19;
+pragma solidity 0.8.17;
 
-contract MyContract {
-    string private name;
+// contract MyContract {
+//     string private name;
 
-    constructor(string memory _name) {
-        name = _name;
+//     constructor(string memory _name) {
+//         name = _name;
+//     }
+
+//     function setName(string memory _name) public {
+//         name = _name;
+//     }
+
+//     function getName() public view returns (string memory) {
+//         return name;
+//     }
+// }
+
+contract Sumfun {
+    // uint256 private input1;
+    // uint256 private input2;
+    uint256 private output;
+
+    constructor() {
+        output = 0;
     }
 
-    function setName(string memory _name) public {
-        name = _name;
+    function add (uint256  _input1, uint256  _input2) public {
+        output = _input1 + _input2;
     }
 
-    function getName() public view returns (string memory) {
-        return name;
+    function getOutput() public view returns (uint256 ) {
+        return output;
     }
 }
